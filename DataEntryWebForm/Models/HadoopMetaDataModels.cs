1740a9fd854e297229c9d72094ba79993b0b38c7
Tommy Durant <thomas.durant@yale.edu>
2016-01-23 14:01:14 -0500

    initial commit

---
+++ DataEntryWebForm/Models/HadoopMetaDataModels.cs
@@ -0,0 +1,28 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataEntryWebForm.Models
+{
+    [ElasticType(Name = "hadoop_metadata")]
+    public class HadoopMetaDataModels
+    {
+        public string Id { get; set; }
+
+        public string ExtractName { get; set; }
+        public string Description { get; set; }
+        public string Requestor { get; set; }
+        public string RequestorEmail { get; set; }
+        public string Request { get; set; }
+        public string[] DataSources { get; set; }
+        public string DataExtractDetails { get; set; }
+
+        // select box; hdfs, hbase, elastic, other
+        public string ClusterStorageLocation { get; set; }
+
+        public string ClusterStoragePath { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
---
+++ DataEntryWebForm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataEntryWebForm.Startup))]
+namespace DataEntryWebForm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
