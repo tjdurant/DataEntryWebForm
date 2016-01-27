410716d72546d465e6f8ab53ad6acad6b60cabfc
Tommy Durant <thomas.durant@yale.edu>
2016-01-27 12:07:17 -0500

    see if this returns index to list

--- DataEntryWebForm/Content/ElasticAPI/ElasticQueries.cs
+++ DataEntryWebForm/Content/ElasticAPI/ElasticQueries.cs
@@ -1,5 +1,6 @@
 ﻿using DataEntryWebForm.DataAccessLayer;
 using DataEntryWebForm.Models;
+using Nest;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -22,48 +23,16 @@ namespace DataEntryWebForm.Content.ElasticAPI
             var indexDetails = new List<HadoopMetaDataModels>();
 
             // read .txt file into string
-            string aggString = File.ReadAllText(queryDslPath);
+            string queryString = File.ReadAllText(queryDslPath);
 
             // run elastic search with raw JSON query string
             var searchResult = ec.Current.Search<HadoopMetaDataModels>(s => s
-                .QueryRaw(aggString)
+                .QueryRaw(queryString)
                 );
 
-            // instantiate bucket of aggregations based on timeInterval 
-            var agBucket = (Bucket)searchResult.Aggregations["my_date_histo"];
-
-            // iterate through each date-item an aggregation was performed on
-            foreach (HistogramItem item in agBucket.Items)
-            {
-                // access the valueMetric for each nested aggregation 
-                var mov_avg = (ValueMetric)item.Aggregations["agg_avg"];
-
-                // convert date_histogram to dateTime object
-                var date = item.Date;
-
-                // convert valueMetric to value
-                var avg_value = mov_avg.Value;
-
-                // pass aggregation data into model 
-                var avgResult = new MovingAverageVals
-                {
-                    Value = avg_value,
-                    Date = date
-                };
-
-                avgList.Add(avgResult);
-            }
-
-            // pass component, timeInterval and avgList into model
-            var cur = new MovingAverageModels
-            {
-                Component = component,
-                TimeResolution = timeInterval,
-                AvgVals = avgList
-            };
-
-            ma.Add(cur);
-            return ma;
+            indexDetails = searchResult.Documents.ToList();
+            
+            return indexDetails;
         }
     }
 }
