684c2f655273ee9fd067a11bfdefafa90ea6c729
Tommy Durant <thomas.durant@yale.edu>
2016-01-28 20:21:46 -0500

    Data table still working

--- DataEntryWebForm/Models/HadoopMetaDataModels.cs
+++ DataEntryWebForm/Models/HadoopMetaDataModels.cs
@@ -1,6 +1,7 @@
 ﻿using Nest;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -13,6 +14,7 @@ namespace DataEntryWebForm.Models
         [ElasticProperty(Name = "_id", NumericType = NumberType.Long)]
         public string Id { get; set; }
 
+        [Required]
         [ElasticProperty(Name = "extract_name")]
         public string ExtractName { get; set; }
 
