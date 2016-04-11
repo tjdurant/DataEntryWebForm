782485488fcb6305cec0be42fad66bb595ffa1fc
Tommy Durant <thomas.durant@yale.edu>
2016-04-11 07:47:23 -0400

    Deleted two unused views and a context class

--- DataEntryWebForm/Models/Constants.cs
+++ DataEntryWebForm/Models/Constants.cs
@@ -7,7 +7,6 @@ namespace DataEntryWebForm.Models
 {
     public class Constants
     {
-
         public const string DEFAULT_INDEX = "hadoop_metadata";
         public const string DEFAULT_INDEX_TYPE = "hadoop_metadata";
         public const string BASIC_DATE = "yyyyMMdd";
